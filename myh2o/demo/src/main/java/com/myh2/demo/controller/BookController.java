c06e0d50a614737923a8ec52ea68c6d13bf8729e
micserv010821 <dotnet290320@gmail.com>
2023-05-11 21:25:32 +0300

    upload

---
+++ myh2o/demo/src/main/java/com/myh2/demo/controller/BookController.java
@@ -0,0 +1,24 @@
+package com.myh2.demo.controller;
+
+import com.myh2.demo.model.Book;
+import com.myh2.demo.repository.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    public Iterable<Book> findall() {
+        return bookRepository.findAll();
+    }
+
+}
